5c6796f4f5b9fbf7b546d70c229743fcd6fb08b7
Minh Tu <66105355+minhtune@users.noreply.github.com>
2024-08-04 12:16:55 +0700

    1.1

--- script.js
+++ script.js
@@ -6,20 +6,20 @@ document.getElementById('calcForm').addEventListener('submit', function(event) {
     var var3 = parseFloat(document.getElementById('var3').value);
 
     var result;
-
+    
     // Chọn công thức dựa trên giá trị của biến thứ ba
     switch(var3) {
         case 5:
             // Công thức cho biến thứ ba là 5
-            result = (var1 + var2) * var3;
+            result = 210000*(var1*var2/10000);
             break;
         case 7:
             // Công thức cho biến thứ ba là 7
-            result = (var1 - var2) / var3;
+            result = 230000*(var1*var2/10000);
             break;
         case 9:
             // Công thức cho biến thứ ba là 9
-            result = (var1 * var2) + var3;
+            result = 300000*(var1*var2/10000);
             break;
         default:
             result = 'Lỗi: Giá trị không hợp lệ';
